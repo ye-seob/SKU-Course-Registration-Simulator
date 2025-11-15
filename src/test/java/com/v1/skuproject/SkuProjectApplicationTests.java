79a27a4614b2f51610191bec314a86261627eab2
ye-seob <jakalob103@naver.com>
2025-11-15 11:38:57 +0000

    first commit

---
+++ src/test/java/com/v1/skuproject/SkuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1.skuproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkuProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
