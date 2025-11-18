441a6dd86261b1618e2fb604d24f66c3540d1e83
ye-seob <jakalob103@naver.com>
2025-11-18 08:01:06 +0000

    feat(#1): Professor 엔티티 작성 완료

--- src/main/java/com/v1/skuproject/domain/Professor.java
+++ src/main/java/com/v1/skuproject/domain/Professor.java
@@ -1,2 +1,21 @@
-package com.v1.skuproject.domain;public class Professor {
+package com.v1.skuproject.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "professor")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
 }
