fad53ed14baf68f815a8f37872622c254f8eb176
ye-seob <jakalob103@naver.com>
2025-11-21 11:38:28 +0000

    refactor(#3): Major 추가

--- src/main/java/com/v1/skuproject/domain/user/Major.java
+++ src/main/java/com/v1/skuproject/domain/user/Major.java
@@ -1,5 +1,7 @@
 package com.v1.skuproject.domain.user;
 
 public enum Major {
-    SOFTWARE
+    SOFTWARE,            // 소프트웨어학과
+    ELECTRONICS_COMPUTER, // 전자컴퓨터공학과
+    BUSINESS             // 경영학과
 }
