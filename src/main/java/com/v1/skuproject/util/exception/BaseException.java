379e94149139398acb353945903e9ea88e7947ce
ye-seob <jakalob103@naver.com>
2025-11-28 15:34:18 +0900

    :sparkles: feature(#25): 예외 커스텀 응답 및 예외 핸들러 구현

---
+++ src/main/java/com/v1/skuproject/util/exception/BaseException.java
@@ -0,0 +1,17 @@
+package com.v1.skuproject.util.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BaseException extends RuntimeException {
+    // RuntimeException을 상속함으로써 컨트롤러나 서비스 계층에서 try-catch로 감싸지 않아도 됨
+
+    private final ErrorCode errorCode; //    ErrorCode : HTTP 상태 코드, 오류 코드, 메시지
+
+    public BaseException(ErrorCode errorCode) {
+
+        super(errorCode.getMessage());
+
+        this.errorCode = errorCode;
+    }
+}
