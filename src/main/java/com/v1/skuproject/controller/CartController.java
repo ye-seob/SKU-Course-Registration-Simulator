03c37447f07e192c68e967569b048ca754cf58e6
ye-seob <jakalob103@naver.com>
2025-12-04 23:54:57 +0900

    :sparkles: feat(#33): CartController 구현

---
+++ src/main/java/com/v1/skuproject/controller/CartController.java
@@ -0,0 +1,75 @@
+package com.v1.skuproject.controller;
+
+import com.v1.skuproject.domain.cart.CartLecture;
+import com.v1.skuproject.service.CartService;
+import com.v1.skuproject.util.response.ApiResponse;
+import com.v1.skuproject.util.response.ResponseHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/carts")
+@Tag(name = "Cart", description = "장바구니 관련 API")
+@RequiredArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+
+
+    @Operation(summary = "장바구니 담기")
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse<CartLecture>> addLectureToCart(
+            Authentication authentication,
+            @RequestParam(name = "lectureId") Long lectureId
+    ) {
+
+        Long userId = (Long) authentication.getPrincipal();
+        log.info("컨트롤러 진입 - 장바구니 담기. userId: {}, lectureId: {}", userId, lectureId);
+
+        CartLecture cartLecture = cartService.addLectureToCart(userId, lectureId);
+
+        log.info("장바구니 담기 성공. CartLecture ID: {}", cartLecture.getId());
+
+        return ResponseHandler.ok(cartLecture);
+    }
+
+
+    @Operation(summary = "장바구니 취소")
+    @DeleteMapping("/remove")
+    public ResponseEntity<ApiResponse<String>> removeLectureFromCart(
+            Authentication authentication,
+            @RequestParam(name = "lectureId") Long lectureId
+    ) {
+        Long userId = (Long) authentication.getPrincipal();
+        log.info("컨트롤러 진입 - 장바구니 강의 삭제. userId: {}, lectureId: {}", userId, lectureId);
+
+        cartService.removeLectureFromCart(userId, lectureId);
+
+        log.info("장바구니 강의 삭제 성공. userId: {}, lectureId: {}", userId, lectureId);
+        return ResponseHandler.ok("장바구니 강의 삭제 성공");
+    }
+
+
+    @Operation(summary = "장바구니 조회")
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<CartLecture>>> getCartLectures(
+            Authentication authentication
+    ) {
+        Long userId = (Long) authentication.getPrincipal();
+        log.info("컨트롤러 진입 - 장바구니 조회. userId: {}", userId);
+
+        List<CartLecture> cartLectures = cartService.getCartLectures(userId);
+
+        log.info("장바구니 조회 성공. 담긴 강의 수: {}", cartLectures.size());
+
+        return ResponseHandler.ok(cartLectures);
+    }
+}
