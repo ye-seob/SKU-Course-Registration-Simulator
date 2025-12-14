dbde39187b0bef4612dc5d742434d20d1dda0f28
ye-seob <jakalob103@naver.com>
2025-12-15 04:24:35 +0900

    :sparkles: feat(#39): Enrollment 도메인 조회 신청 취소 엔티티 및 DTO 구현

--- src/main/java/com/v1/skuproject/dto/enrollment/EnrollmentResponse.java
+++ src/main/java/com/v1/skuproject/dto/enrollment/EnrollmentResponse.java
@@ -2,6 +2,9 @@ package com.v1.skuproject.dto.enrollment;
 
 import com.v1.skuproject.domain.enrollment.Enrollment;
 import com.v1.skuproject.domain.enrollment.EnrollmentStatus;
+import com.v1.skuproject.domain.lecture.Lecture;
+import com.v1.skuproject.domain.lecture.LectureType;
+import com.v1.skuproject.domain.user.Major;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,16 +14,54 @@ import java.time.LocalDateTime;
 @Builder
 public class EnrollmentResponse {
 
+    // 수강신청 상태
     private EnrollmentStatus status; // SUCCESS, FAIL, PENDING
     private String reason;           // 실패 사유
     private LocalDateTime createdAt; // 신청 시각
 
+    // 강의 정보
+    private Long lectureId;
+    private String lectureName;
+    private String professor;
+    private String lectureCode;
+    private Integer classNumber;
+    private LectureType type;
+    private Double credit;
+    private Double time;
+    private String schedule;
+    private Major major;
+    private Integer enrollment; // 현재 수강 인원
+    private Integer capacity;   // 총 정원
+    private Double rating;      // 강의 평점
+
     public static EnrollmentResponse from(Enrollment e) {
+        Lecture l = e.getLecture();
+
         return EnrollmentResponse.builder()
                 .status(e.getStatus())
-                .reason(e.getReason())
+                .reason(null)
                 .createdAt(e.getCreatedAt())
+                .lectureId(l.getId())
+                .lectureName(l.getLectureName())
+                .professor(l.getProfessor())
+                .lectureCode(l.getLectureCode())
+                .classNumber(l.getClassNumber())
+                .type(l.getType())
+                .credit(l.getCredit())
+                .time(l.getTime())
+                .schedule(l.getSchedule())
+                .major(l.getMajor())
+                .enrollment(l.getEnrollment())
+                .capacity(l.getCapacity())
+                .rating(l.getRating())
                 .build();
     }
 
+    public static EnrollmentResponse fail(String reason) {
+        return EnrollmentResponse.builder()
+                .status(EnrollmentStatus.FAIL)
+                .reason(reason)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
 }
