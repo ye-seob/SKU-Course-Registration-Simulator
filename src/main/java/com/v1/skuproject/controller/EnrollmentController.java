0146617f08aa30047fc17489061e6ff28fde26ee
ye-seob <jakalob103@naver.com>
2025-12-29 22:43:25 +0900

    :recycle: refactor(#42): 기존 수강신청 api 삭제

--- src/main/java/com/v1/skuproject/controller/EnrollmentController.java
+++ src/main/java/com/v1/skuproject/controller/EnrollmentController.java
@@ -2,6 +2,7 @@ package com.v1.skuproject.controller;
 
 import com.v1.skuproject.dto.enrollment.EnrollmentResponse;
 import com.v1.skuproject.service.EnrollmentService;
+import com.v1.skuproject.service.QueueService;
 import com.v1.skuproject.util.response.ApiResponse;
 import com.v1.skuproject.util.response.ResponseHandler;
 import io.swagger.v3.oas.annotations.Operation;
@@ -23,26 +24,13 @@ import java.util.List;
 public class EnrollmentController {
 
     private final EnrollmentService enrollmentService;
+    private final QueueService queueService;
 
     @Operation(summary = "수강 신청 조회", description = "사용자가 신청한 강의 내역 조회")
     @GetMapping("/list")
     public ResponseEntity<ApiResponse<List<EnrollmentResponse>>> list(Authentication authentication) {
         Long userId = (Long) authentication.getPrincipal();
         List<EnrollmentResponse> response = enrollmentService.getEnrollments(userId);
-        return ResponseHandler.ok(response);
-    }
-
-    @Operation(summary = "수강 신청", description = "강의 신청")
-    @PostMapping("/enroll")
-    public ResponseEntity<ApiResponse<EnrollmentResponse>> enroll(
-            Authentication authentication,
-            @RequestParam(name = "lectureId") Long lectureId
-    ) {
-        Long userId = (Long) authentication.getPrincipal();
-
-        EnrollmentResponse response = enrollmentService.enroll(userId, lectureId);
-
-
         return ResponseHandler.ok(response);
     }
 
