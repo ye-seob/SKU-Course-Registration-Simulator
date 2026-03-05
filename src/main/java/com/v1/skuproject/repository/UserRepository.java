714a52453346b96a27f38cb895a01161ec1b48cc
ye-seob <jakalob103@naver.com>
2026-03-05 16:22:24 +0900

    :recycle: refactor(#78): studentId 타입 변경 (Long -> String)

--- src/main/java/com/v1/skuproject/repository/UserRepository.java
+++ src/main/java/com/v1/skuproject/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    Optional<User> findByStudentId(long studentId);
-    Boolean existsByStudentId(long studentId);
+    Optional<User> findByStudentId(String studentId);
+    Boolean existsByStudentId(String studentId);
 
 }
